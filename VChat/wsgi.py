de0603e5522b3f115ca26a168f572212db344d3d
Vaibhav Gautam <vaibhavgautam801@gmail.com>
2024-09-02 21:55:14 +0530

    Host on Vercel

--- VChat/wsgi.py
+++ VChat/wsgi.py
@@ -7,12 +7,15 @@ For more information on this file, see
 https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
 
-import os
+import os,sys
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'VChat.settings')
 
 application = get_wsgi_application()
+
+if os.environ.get('ENV') == 'production':
+    sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 
 app = application
